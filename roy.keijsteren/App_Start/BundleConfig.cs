c59317a62c01cf39326496ee822c7d82919d2784
Rakeij <roy_vankeysteren@hotmail.com>
2017-09-05 17:00:59 +0200

    init

---
+++ roy.keijsteren/App_Start/BundleConfig.cs
@@ -0,0 +1,60 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace roy.keijsteren
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui-{version}.js",
+                        "~/Scripts/plugin.js",
+                        "~/Scripts/jquery-min.js",
+                        "~/Scripts/scripts.js"
+                        ));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/devblog").Include(
+                        "~/Scripts/syntax/shCore.css",
+                        "~/Scripts/syntax/shThemeDefault.css",
+                        "~/Content/font-awesome/css/font-awesome.css"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/devblogJS").Include(
+                  "~/Scripts/syntax/shBrushCss.js",
+                  //"~/Scripts/syntax/ShBrushJscript.js",
+                  //"~/Scripts/syntax/shBrushPerl.js",
+                  //"~/Scripts/syntax/shBrushPhp.js",
+                  //"~/Scripts/syntax/shBrushPlain.js",
+                  //"~/Scripts/syntax/shBrushPython.js",
+                  //"~/Scripts/syntax/shBrushRuby.js",
+                  //"~/Scripts/syntax/shBrushSql.js",
+                  //"~/Scripts/syntax/shBrushVb.js",
+                  //"~/Scripts/syntax/shBrushXml.js",
+                  "~/Scripts/syntax/shCore.js"
+                  ));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/plugin.css",
+                      "~/Content/style.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ roy.keijsteren/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("roy.keijsteren")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Actemium")]
+[assembly: AssemblyProduct("roy.keijsteren")]
+[assembly: AssemblyCopyright("Copyright © Actemium 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("95d0305e-b27a-44de-9044-754cd41d3af5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
